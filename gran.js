aa193d9d5ed8eecfcc3564eaea05466224e5cf35
Adetayor <adetayo456@gmail.com>
2025-10-08 05:12:08 +0100

    Gran js added

--- gran.js
+++ gran.js
@@ -1,3 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
+    const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
+
+    toggleButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active');
+    });   
+});
+
+
 const button = document.querySelector('#button');
 const heading = document.querySelector('h1');
 
